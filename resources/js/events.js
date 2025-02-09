52785b8d4ca33bea27bd52a889b7f8f5320bba5d
AlevtynaShuhai <Sherbanalevtina@gmail.com>
2025-02-09 20:49:49 +0200

    nature.js

--- resources/js/events.js
+++ resources/js/events.js
@@ -254,6 +254,26 @@ export const events = [
         modalIncludes: null,
         modalDiscount: null,
     },
+    {
+        id: 16,
+        eventName: 'ACRYLIC PAINTING COURSE TEXTURES AROUND US',
+        startDate: 'February 23 -',
+        date: '2025-03-23',
+        time: '10:00 am - 01:00 pm',
+        day: "Sundays, ",
+        price: 300,
+        description: 'All painting supplies provided + snacks and drinks',
+        img: '2025.jpg',
+        modalDescription: 'Turn nature into art with this acrylic painting course!Take your skills to the next level and discover the beauty of nature through acrylic painting.' +
+            '<div class="modal-body-description-bold">\n' +
+            '                                In the course of acrylic painting “Textures around us” during five sessions we will study techniques to create natural textures, from the softness of clouds to the roughness of a rocky mountain:\n' +
+            '                            </div>\n' +
+            '                            - How to convey the depth and texture of water, clouds, stone, wheat field, fire <br>\n' +
+            '                            - Special methods of working with acrylics <br>\n' +
+            '                            - Painting using a variety of tools to create realistic natural images <br>\n',
+        modalIncludes: null,
+        modalDiscount: null,
+    }
 ];
 
 export const infinityEvent = [
